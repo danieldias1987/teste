d6bac5533fe4b744af975958a805ce7f03b28d56
danieldias1987 <danieldias_1987@hotmail.com>
2019-06-25 10:45:12 +0100

    exercicios

---
+++ src/teste/Conversao.java
@@ -0,0 +1,49 @@
+package teste;
+
+public class Conversao {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 
+		double valor = 3.1415;
+		int outrovalor = valor;
+		
+		inn valornovo = 3.14;
+		
+		double d = 5;
+		int i = d;
+		
+		long x = 10000;
+		int i = x;
+		
+			
+		int j = 5;
+		double d2 = j;
+		
+		
+		double d3 = 3.14;
+		int i = (int) d3;
+		
+		
+		long x = 9223372036854775807L;
+		int i = (int) x;
+		
+		
+		long x = 10000;
+		int i = (int) x;
+		
+		float x = 0.0;// solucao seria float x = 0.0f;
+				*/
+		
+		double d = 5;
+		float f = 3;
+		
+		float x = f + (float) d;
+		
+		System.out.println(x);
+		
+		
+	}
+
+}
---
+++ src/teste/Wrappers.java
@@ -0,0 +1,29 @@
+package teste;
+
+public class Wrappers {
+	public static void main(String[] args) {
+		
+		Boolean boolean1 = new Boolean (true);
+		
+		Byte byte1 = new Byte ("1");
+		
+		Character charcter1 = new Character('f');
+		
+		Double double1 = new Double (2.2);
+		
+		Float Float1 = new Float (3.40);
+		
+		Integer int1 = new Integer ("1");
+		
+		Long long1 = new Long ("922");
+		
+		Short short1 = new Short("327");
+		
+		System.out.println(charcter1);
+		
+		
+		
+	}
+	
+}
+
